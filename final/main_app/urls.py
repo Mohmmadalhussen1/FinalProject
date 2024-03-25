9d3e53942923ea156910dcad57c94dbff38d525e
mohmmad alhussen <mohmmadalhussen1@gmail.com>
2024-03-25 15:04:32 +0300

    second chnage

--- final/main_app/urls.py
+++ final/main_app/urls.py
@@ -6,5 +6,8 @@ app_name = "main_app"
 urlpatterns = [
         path("", views.index, name="index"),
         path("controls", views.controls, name="controls"),
-    
+    path("attack", views.attack_navigator, name="attack_navigator"),
+    # Add any other URL patterns as needed
+
+
 ]
--- final/main_app/views.py
+++ final/main_app/views.py
@@ -14,3 +14,37 @@ def controls(request:HttpRequest):
 # Home HTML
 
     return render(request, "main_app/controls.html")
+
+from django.http import HttpRequest
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpRequest
+from django.shortcuts import render
+from django.urls import reverse
+
+def attack_navigator(request: HttpRequest):
+    Domains = [
+        'Domain: Cybersecurity Governance',
+        'Domain: Cybersecurity Defense',
+        'Domain: Cybersecurity Resilience',
+        'Domain: Third-Party and Cloud Computing Cybersecurity',
+        'Domain: ICS Cybersecurity (Industrial Control Systems)',
+    ]
+    index_url = reverse("main_app:index")
+
+    # Generating URLs for Govcons and Defcons pointing to the index page
+    govcons = [
+        ('1-1 Cybersecurity Strategy', 'controls'),
+        ('1-2 Cybersecurity Management', ''),
+        ('1-3 Cybersecurity Policies and Procedures', ''),
+        ('1-4 Cybersecurity Roles and Responsibilities', ''),
+    ]
+    
+    defcons = [
+        ('1-1 Cybersecurity Strategy', ''),
+        ('1-2 Cybersecurity Management', ''),
+        ('1-3 Cybersecurity Policies and Procedures', ''),
+        ('1-4 Cybersecurity Roles and Responsibilities', ''),
+    ]
+
+    return render(request, 'main_app/attack_navigator.html', {'Domains': Domains, 'Govcons': govcons, 'Defcons': defcons})
