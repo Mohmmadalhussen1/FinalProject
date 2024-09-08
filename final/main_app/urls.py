d7f23ba07c956f0cf8b38219de098d630d67460b
mohmmad alhussen <mohmmadalhussen1@gmail.com>
2024-09-08 19:35:31 +0300

    new project

--- final/main_app/urls.py
+++ final/main_app/urls.py
@@ -4,8 +4,13 @@ from . import views
 app_name = "main_app"
 
 urlpatterns = [
-    path('auditor_workflow/', views.auditor_workflow, name='auditor_workflow'),
-     path('program_interaction/', views.program_interaction, name='program_interaction'),
-    path('update_controls/', views.update_controls, name='update_controls'),
-    path('', views.control_tree, name='control_tree'),
+    path('sign_up_page/', views.sign_up_page, name='sign_up_page'),
+    path('sessions_page/', views.sessions_page, name='sessions_page'),
+    path('report_generation_page/', views.report_generation_page, name='report_generation_page'),
+    path('login_page/', views.login_page, name='login_page'),
+    path('policy_review/', views.policy_review, name='policy_review'),
+    path('tree/', views.tree_page, name='tree'),
+    path('', views.home_page, name='home_page'),
+    path('signout/', views.signout_page, name='signout_page'),
+
 ]
--- final/main_app/views.py
+++ final/main_app/views.py
@@ -1,19 +1,54 @@
-from django.http import HttpRequest
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User, Permission, Group
+from django.contrib.auth import authenticate, login, logout
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.http import JsonResponse
 # Create your views here.
 # views.py
 
-def auditor_workflow(request):
-    return render(request, 'main_app/auditor_workflow.html')
+def home_page(request):
+    return render(request, 'main_app/home_page.html')
 
-def program_interaction(request):
-    return render(request, 'main_app/program_interaction.html')
+def sessions_page(request):
+    return render(request, 'main_app/sessions_page.html')
+
+def report_generation_page(request):
+    return render(request, 'main_app/report_generation_page.html')
+
+def policy_review(request):
+    return render(request, 'main_app/policy_review.html')
+
+def sign_up_page(request):
+    return render(request, 'main_app/sign_up_page.html')
+
+def tree_page(request):
+    return render(request, 'main_app/tree.html')
+
+def login_page(request: HttpRequest):
+    if request.user.is_authenticated:
+        print(request.user.username)
+        return redirect("main_app:home_page")
+    # if user login already redirect to home page
+
+    msg = None
+    if request.method == "POST":
+        user: User = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        if user:
+            login(request, user)
+            return redirect("main_app:home_page")
+        else:
+            msg = "Incorrect Credentials"
+
+    return render(request, "main_app/login_page.html", {"msg": msg})
 
 
-
-def update_controls(request):
-    return render(request, 'main_app/update_controls.html')
-
-def control_tree(request):
-    return render(request, 'main_app/control_tree.html')
+def signout_page(request):
+    logout(request)
+    return redirect('main_app:login_page')  # Redirect to login page after sign out
